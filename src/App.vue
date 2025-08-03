e65d972ec0b25fc0abf2d8c7ae3759f4f163e590
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-03 20:44:34 +0300

    feat:consume api backend

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ const name = 'teguh bagmar'
 </script>
 
 <template>
-  <h1>Hellow world</h1>
+  <h1 class="text-3xl text-red-500">Hellow world</h1>
   <MainButton :title="`submit`" />
   <MainButton :disabled="true" />
   <p>{{ name }}</p>
